a53f2e78ef932ef8b4edca1a71ce826232b8afc4
DmitrySmyslov1C <87122109+DmitrySmyslov1C@users.noreply.github.com>
2021-07-13 20:11:37 +0300

    Update task.js

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -15,19 +15,28 @@ function averageMark(a, g, p) {
 	
     sred = (algebra + geography + physics) / 3;	
 	
-	return sred// ваш ответ;
+	return sred;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+    let userName = userName;
+	
+   message = "Привет, мир! Меня зовут " +  userName;	
+	
+	
+	return message;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+    let x = 2;
+    let y = 22;
+    let z = 0;	
+	
+    result = x * y + 5 * z + x - 1;
+	
+    return result;
 }
